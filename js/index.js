4a66128ca5240bcee77661e285d4255af750dd9e
Clement Ou <clement.h.ou@gmail.com>
2019-09-29 21:18:14 -0700

    fixed crash

--- js/index.js
+++ js/index.js
@@ -13,31 +13,33 @@ client.on('ready', () => {
         console.log(`Logged in as ${client.user.tag}!`))
 });
 
-let isReady = true;
 client.on('voiceStateUpdate', (_, newMember) => {
+    const voiceChannel = newMember.voiceChannel;
     console.log('Voice State Update');
     let user = ids.users.filter(user => user.id === newMember.id)[0]; // may be undefined
 
-    if (user === undefined || newMember.voiceChannel === undefined || !isReady) return; // stop if not
+    if (user === undefined || voiceChannel === undefined
+        // || client.voiceConnections.find(voiceChannel.connection)
+        )
+        return; // stop if not
     console.log(newMember.user + 'state updated');
 
     // TEXT
     client.channels.get(GENERAL_ID).send(newMember.displayName +
-        ` ALERT IN VOICE CHANNEL ***<#${newMember.voiceChannel.id}>!***`).then(_ =>
+        ` ALERT IN VOICE CHANNEL ***<#${voiceChannel.id}>!***`).then(_ =>
         console.log('Message Sent to ' + client.channels.get(GENERAL_ID)));
 
     // VOICE
-    newMember.voiceChannel.join().then(connection => {
-        console.log('Connection Established to vc ' + newMember.voiceChannel.name);
+    voiceChannel.join().then(connection => {
+        console.log('Connection Established to vc ' + voiceChannel.name);
 
         let dispatcher = connection.playFile(user.path);
-        console.log('Audio Playing in ' + newMember.voiceChannel.name);
-        isReady = false;
+        console.log('Audio Playing in ' + voiceChannel.name);
+
 
         dispatcher.on('end', end => {
             console.log('Audio Finished Playing');
-            isReady = true;
-            newMember.voiceChannel.leave();
+            voiceChannel.leave();
         });
     });
 });
