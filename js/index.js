da2dcdc2a29017be844905e2834a02879350af46
Clement Ou <clement.h.ou@gmail.com>
2019-09-29 21:43:52 -0700

    fix???

--- js/index.js
+++ js/index.js
@@ -1,14 +1,12 @@
-console.log("Startup");
-
 const commando = require('discord.js-commando'); // importing discord.js
 const client = new commando.Client();
 
-const ids = require('../ids.json');
+const ids = require('../ids.json'); // client ids, soundbites, channel ids
 const GENERAL_ID = ids.channels[0]["log_channel"];
 
 sleep = (ms) => new Promise(res => setTimeout(res, ms));
 
-client.on('ready', () => {
+client.on('ready', () => { // log in
     client.channels.get(GENERAL_ID).send(`Logged in as ${client.user.tag}!`).then(_ =>
         console.log(`Logged in as ${client.user.tag}!`))
 });
@@ -19,9 +17,7 @@ client.on('voiceStateUpdate', (_, newMember) => {
     let user = ids.users.filter(user => user.id === newMember.id)[0]; // may be undefined
 
     if (user === undefined || voiceChannel === undefined
-        // || client.voiceConnections.find(voiceChannel.connection)
-        )
-        return; // stop if not
+        || client.voiceConnections.array().includes(voiceChannel.connection)) return; // stop if not
     console.log(newMember.user + 'state updated');
 
     // TEXT
@@ -30,19 +26,18 @@ client.on('voiceStateUpdate', (_, newMember) => {
         console.log('Message Sent to ' + client.channels.get(GENERAL_ID)));
 
     // VOICE
-    voiceChannel.join().then(connection => {
+    voiceChannel.join().then(connection => { // connect to vc
         console.log('Connection Established to vc ' + voiceChannel.name);
 
-        let dispatcher = connection.playFile(user.path);
+        let dispatcher = connection.playFile(user.path); // play audio
         console.log('Audio Playing in ' + voiceChannel.name);
 
-
-        dispatcher.on('end', end => {
+        dispatcher.on('end', end => { // audio finishes
             console.log('Audio Finished Playing');
             voiceChannel.leave();
         });
     });
 });
 
-client.login(process.env.BOT_TOKEN).then(r => {
+client.login(process.env.BOT_TOKEN).then(r => { // login with token
 });
