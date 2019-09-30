50c49f022ac19c4adeeffa3c50b733ac25ce9bd2
Clement Ou <clement.h.ou@gmail.com>
2019-09-29 22:06:43 -0700

    logging

--- js/index.js
+++ js/index.js
@@ -2,38 +2,38 @@ const commando = require('discord.js-commando'); // importing discord.js
 const client = new commando.Client();
 
 const ids = require('../ids.json'); // client ids, soundbites, channel ids
-const GENERAL_ID = ids.channels[0]["log_channel"];
+const logGuildID = ids.channels[0]["server"];
+const logChannelID = ids.channels[0]["log_channel"];
 
-sleep = (ms) => new Promise(res => setTimeout(res, ms));
+sleep = (ms) => new Promise(res => setTimeout(res, ms)); // TODO add catch
 
 client.on('ready', () => { // log in
-    client.channels.get(GENERAL_ID).send(`Logged in as ${client.user.tag}!`).then(_ =>
+    client.channels.get(logChannelID).send(`Logged in as ${client.user.tag}!`).then(_ =>
         console.log(`Logged in as ${client.user.tag}!`))
 });
 
 client.on('voiceStateUpdate', (_, newMember) => {
     const voiceChannel = newMember.voiceChannel;
-    console.log('Voice State Update');
     let user = ids.users.filter(user => user.id === newMember.id)[0]; // may be undefined
 
     if (user === undefined || voiceChannel === undefined
         || client.voiceConnections.array().includes(voiceChannel.connection)) return; // stop if not
-    console.log(newMember.user + 'state updated');
+    console.log('@' + newMember.displayName + ': state updated');
 
     // TEXT
-    client.channels.get(GENERAL_ID).send(newMember.displayName +
+    client.channels.get(logChannelID).send(newMember.displayName +
         ` ALERT IN VOICE CHANNEL ***<#${voiceChannel.id}>!***`).then(_ =>
-        console.log('Message Sent to ' + client.channels.get(GENERAL_ID)));
+        console.log('#' + client.guilds.get(logGuildID).channels.get(logChannelID).name) + ': message sent');
 
     // VOICE
     voiceChannel.join().then(connection => { // connect to vc
-        console.log('Connection Established to vc ' + voiceChannel.name);
+        console.log('#' + voiceChannel.name + ': connection established');
 
         let dispatcher = connection.playFile(user.path); // play audio
-        console.log('Audio Playing in ' + voiceChannel.name);
+        console.log('#' + voiceChannel.name + ': audio playing');
 
         dispatcher.on('end', end => { // audio finishes
-            console.log('Audio Finished Playing');
+            console.log('#' + voiceChannel.name + ': audio finished playing');
             voiceChannel.leave();
         });
     });
