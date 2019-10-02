73d8a80c77bfa2334469b97686767e96598aecee
Clement Ou <clement.h.ou@gmail.com>
2019-10-02 10:47:01 -0700

    fixes, documentation, new sounds
    author Kabir Kwatra <kabir@kwatra.me> 1569811706 -0700
    Co-authored-by: stovenh
    committer Clement Ou <clement.h.ou@gmail.com> 
    
    Way Simpler.
    
    idk if this will work
    
    stoopid
    
    fixed crash
    
    fix???
    
    logging
    
    oop
    
    updated package.json
    
    Add files via upload
    
    Update ids.json
    
    Add files via upload
    
    Update ids.json
    
    Add files via upload
    
    Update ids.json
    
    smol
    
    Delete Quinn.mp3
    Add files via upload
    Add files via upload
    Update ids.json
    Update ids.json
    am lazy
    Update ids.json
    Add files via upload
    Update ids.json
    
    kabir 👳

--- js/index.js
+++ js/index.js
@@ -1,50 +1,43 @@
-console.log("Startup");
-
 const commando = require('discord.js-commando'); // importing discord.js
 const client = new commando.Client();
 
-const ids = require('../ids.json');
-const GENERAL_ID = ids.channels[0]["log_channel"];
+const ids = require('../ids.json'); // client ids, soundbites, channel ids
+const logGuildID = ids.channels[0]["server"];
+const logChannelID = ids.channels[0]["log_channel"];
 
-sleep = (ms) => new Promise(res => setTimeout(res, ms));
+sleep = (ms) => new Promise(res => setTimeout(res, ms)); // TODO add catch
 
-client.on('ready', () => {
-    client.channels.get(GENERAL_ID).send(`Logged in as ${client.user.tag}!`).then(_ =>
+client.on('ready', () => { // log in
+    client.channels.get(logChannelID).send(`Logged in as ${client.user.tag}!`).then(_ =>
         console.log(`Logged in as ${client.user.tag}!`))
 });
 
 client.on('voiceStateUpdate', (_, newMember) => {
-    console.log('Voice State Update');
-    let id_exists = false;
-    let index = '';
-    for (let i = 0; i < ids.users.length; i++) // check if newMember in json
-        if (ids.users[i].id === newMember.id) {
-            id_exists = true;
-            index = i;
-        }
-    if (!id_exists || newMember.voiceChannel === undefined) return; // stop if not
-    console.log(newMember.user + 'state updated');
+    const voiceChannel = newMember.voiceChannel;
+    let user = ids.users.filter(user => user.id === newMember.id)[0]; // may be undefined
+
+    if (user === undefined || voiceChannel === undefined
+        || client.voiceConnections.array().includes(voiceChannel.connection)) return; // stop if not
+    console.log('@' + newMember.displayName + ': state updated');
 
     // TEXT
-    client.channels.get(GENERAL_ID).send(newMember.nickname +
-        ` ALERT IN VOICE CHANNEL ***<#${newMember.voiceChannel.id}>!***`).then(_ =>
-        console.log('Message Sent to ' + client.channels.get(GENERAL_ID)));
+    client.channels.get(logChannelID).send(newMember.displayName +
+        ` ALERT IN VOICE CHANNEL ***<#${voiceChannel.id}>!***`).then(_ =>
+        console.log('#' + client.guilds.get(logGuildID).channels.get(logChannelID).name + ': message sent'));
 
     // VOICE
-    newMember.voiceChannel.join().then(connection => {
-        console.log('Connection Established to vc ' + newMember.voiceChannel.name);
+    voiceChannel.join().then(connection => { // connect to vc
+        console.log('#' + voiceChannel.name + ': connection established');
 
-        let dispatcher = connection.playFile(ids.users[index].path);
-        console.log('Audio Playing in ' + newMember.voiceChannel.name);
+        let dispatcher = connection.playFile(user.path); // play audio
+        console.log('#' + voiceChannel.name + ': audio playing');
 
-        dispatcher.on('speaking', (isSpeaking) => {
-            if (!isSpeaking) {
-                console.log('Audio Finished Playing');
-                newMember.voiceChannel.leave();
-            }
+        dispatcher.on('end', end => { // audio finishes
+            console.log('#' + voiceChannel.name + ': audio finished playing');
+            voiceChannel.leave();
         });
     });
 });
 
-client.login(process.env.BOT_TOKEN).then(r => {
+client.login(process.env.BOT_TOKEN).then(r => { // login with token
 });
